8ed89cfa878f770fba546bc23d74b80566b3ceb2
Nicolás <nhurtado@miuandes.cl>
2017-10-12 01:49:31 -0300

    Se arreglo pequeño error y se ordeno el codigo.

--- main.c
+++ main.c
@@ -16,59 +16,45 @@ how to use the page table and disk interfaces.
 #include <errno.h>
 const char *algth;
 struct disk *disk;
+int *frames_t;
 
 void page_fault_handler( struct page_table *pt, int page )
 {
 	
 	printf("page fault on page #%d\n",page);
-	
 	int nframes = page_table_get_nframes(pt);
-	int * marcos = malloc(nframes*sizeof(int));
-	char * physmem = page_table_get_physmem(pt);
-	//char * virtmem = page_table_get_virtmem(pt);
-	for (int i = 0; i<nframes;i++){
-		marcos[i]=-1;
-	}
+	char *physmem = page_table_get_physmem(pt);
+	int frame = 0; //Marco donde se cargara la pagina.
+
+	//Se elige el marco dependiendo del algoritmo.
 	if(!strcmp(algth,"rand")) {
-		long int rp = lrand48()%nframes;
-		printf("acaaa %ld \n",rp);
-
-		if (marcos[rp]!=-1){
-	
-			disk_write(disk, page, &physmem[rp] );						
-		}
-		marcos[rp]=page;
-		
-		page_table_set_entry(pt,page,rp,PROT_WRITE);
-		
-		page_table_print(pt);
- 
-		
-
-	} else if (!strcmp(algth,"fifo")){
-		int rp = page%nframes;			
-		if (marcos[rp]!=-1){
-			disk_write(disk, page, &physmem[rp] );						
-		}	
-		marcos[rp]=page;
-		
-		page_table_set_entry(pt,page,rp,PROT_WRITE);
-		
-		page_table_print(pt);
-
+		frame = lrand48()%nframes;	
+	}else if (!strcmp(algth,"fifo")){
+		frame = page%nframes;
 	}else if (!strcmp(algth,"custom")){
 
 	}else{
 		fprintf(stderr,"unknown algorithm: %s\n",algth);
 		exit(1);
 	}
+
+	//Si hay una pagina cargada en el marco, la escribo al disco.
+	if (frames_t[frame]!=-1){
+		disk_write(disk, frames_t[frame], &physmem[frame]);						
+	}
+
+	//Leo la pagina del disco y la cargo en el marco.
+	disk_read(disk, page, &physmem[frame]);
+	frames_t[frame]=page;
+	page_table_set_entry(pt,page,frame,PROT_WRITE);
+	page_table_print(pt);
 }
 
 int main( int argc, char *argv[] )
 {
 	if(argc!=5) {
 		/* Add 'random' replacement algorithm if the size of your group is 3 */
-		printf("use: virtmem <npages> <nframes> <lru|fifo> <sort|scan|focus>\n");
+		printf("use: virtmem <npages> <nframes> <rand|fifo> <sort|scan|focus>\n");
 		return 1;
 	}
 
@@ -76,8 +62,12 @@ int main( int argc, char *argv[] )
 	int nframes = atoi(argv[2]);
 	algth = argv[3];
 	const char *program = argv[4];
-	lrand48();
-	
+
+	frames_t = malloc(nframes*sizeof(int)); //Tabla de largo del numero de marcos. Guarda que pagina esta cargada en cada marco.
+	//En un principio, la cargamos con -1.
+	for (int i = 0; i<nframes;i++){
+		frames_t[i]=-1;
+	}
 
 	disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
